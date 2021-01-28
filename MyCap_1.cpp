8d3a1bfd7953a840ec20a688d30283459292a044
esha20 <72601896+esha20@users.noreply.github.com>
2021-01-28 16:50:01 +0530

    codes to get the size of data types and for product of two float numbers.

---
+++ MyCap_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main ()
+{
+using namespace std;
+cout <<"Size of char : "<< sizeof(char)<<"byte";
+cout <<endl;
+cout <<"Size of int : "<< sizeof(int)<<"byte";
+cout <<endl;
+cout <<"Size of float : "<< sizeof(float)<<"byte";
+cout <<endl;
+cout <<"Size of double : "<< sizeof(double)<<"byte";
+cout <<endl;
+
+return 0;
+}
---
+++ MyCap_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main ()
+{
+using namespace std;
+ float a = 3.4, b= 5.5 , product;
+ 
+ cout<<"Enter two numbers : "<< a << b;
+ cout<<endl;
+ product = a*b;
+ cout <<"Product" <<product;
+ return 0;
+}
