2db89a879d4d4bbda88d4c8ab7592c4747b64824
esha20 <72601896+esha20@users.noreply.github.com>
2021-01-30 13:57:18 +0530

    4th task array and pointers

---
+++ MyCap_4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int arr[5];
+	cout <<"Enter elements : ";
+	for (int i=1;i<6;++i)
+	cout << i<<endl;	
+	
+	cout <<"You entered : ";
+	for (int i=0;i<5; ++i)
+	{
+		cout<< *arr+i;
+	}
+	return 0;
+}
